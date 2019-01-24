e7e38926a74d01445c069e05b56f4a311b07fa17
Alfatha Fitrah Insan <elfathfitrah@gmail.com>
2019-01-24 16:31:34 +0700

    Add files via upload

---
+++ Web Code/docs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SEKOLAH</title>
+</head>
+<body style="background-color: #00897b">
+
+<?php require_once('navbar.php'); ?>
+	<div style="width:950px height: 750px">
+		
+	</div>
+	<div class="row">
+    <div class="col s12"><p>s12</p></div>
+    <div class="col s12 m4 l1"></div>
+    <div class="col s12 m4 l8"><iframe src="https://docs.google.com/document/d/e/2PACX-1vR04hg6vN3_VzjmhEdJrkMb6cKSOCp0g7BGjth6vcLaNF3_566-m8KVwdHkJaCNUJzSCwrWgBmcOXrM/pub?embedded=true" width="1050px" height= "750px"></iframe></div>
+    <div class="col s12 m4 l2"><p>s12 m4</p></div>
+  </div>
+	
+	<?php require_once('footer.php'); ?>
+</body>
+
+</html>
+<!--https://www.youtube.com/watch?v=bXw5VOEJNAo
+https://www.youtube.com/watch?v=HuuBk3XbKdQ
+https://www.youtube.com/watch?v=1IkPAstIvus
+https://www.youtube.com/watch?v=_jDPBmn_Tlw&feature=youtu.be
+https://www.facebook.com/haje.agustian/videos/1276137788055/
+https://www.youtube.com/watch?v=Sy1bMKgW7nM-->
